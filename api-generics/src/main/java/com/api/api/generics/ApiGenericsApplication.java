bad35b88b943d7902ab033a34c7390a00254a15b
Rafel <ralfrr55@gmail.com>
2022-11-09 18:06:18 -0300

    employee entity

---
+++ api-generics/src/main/java/com/api/api/generics/ApiGenericsApplication.java
@@ -0,0 +1,13 @@
+package com.api.api.generics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGenericsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGenericsApplication.class, args);
+	}
+
+}
---
+++ api-generics/src/test/java/com/api/api/generics/ApiGenericsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.api.generics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGenericsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
